269d59ff8802eda12be7d99c5d2aa2883a88dc83
tuanha <tuanha@nissho-vn.com>
2022-06-28 10:15:16 +0700

    init

---
+++ src/crawl-data/Crawl.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Action from './button/Action';
+import './Crawl.css'
+
+function Crawl() {
+    return (
+      <div className="Crawl">
+        <React.StrictMode>
+            <Action />
+        </React.StrictMode>
+      </div>
+    );
+  }
+  
+export default Crawl;
+  
---
+++ src/crawl-data/button/Action.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Action() {
+    return (
+      <div className="Action">
+        
+      </div>
+    );
+  }
+  
+export default Action;
